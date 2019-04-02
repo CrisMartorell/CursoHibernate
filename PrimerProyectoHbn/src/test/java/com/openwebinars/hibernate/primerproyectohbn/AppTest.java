4b303c604d41df4e6d80da150d8e66a44824c329
Cristina Martorell López <cristina.martorelllopez@plexus.local>
2019-04-02 11:38:01 +0200

    Project made

---
+++ PrimerProyectoHbn/src/test/java/com/openwebinars/hibernate/primerproyectohbn/AppTest.java
@@ -0,0 +1,20 @@
+package com.openwebinars.hibernate.primerproyectohbn;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
