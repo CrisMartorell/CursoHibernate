1f4a6a1acd4bbb628e4f4e83f602f6ab6117e0e1
Cristina Martorell López <cristina.martorelllopez@plexus.local>
2019-04-02 12:23:23 +0200

    SessionFactory made

--- PrimerProyectoHbn/src/main/java/com/openwebinars/hibernate/primerproyectohbn/App.java
+++ PrimerProyectoHbn/src/main/java/com/openwebinars/hibernate/primerproyectohbn/App.java
@@ -1,5 +1,12 @@
 package com.openwebinars.hibernate.primerproyectohbn;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    	StandardServiceRegistry sr = new StandardServiceRegistryBuilder().configure().build();
+    	
+    	SessionFactory sf = new MetadataSources(sr).buildMetadata().buildSessionFactory();
+    	
+    	
+    	//Código legacy
+    	//SessionFactory sf = new Configuration().configure().buildSessionFactory();
+    	
+    	//Apertura de la sesión
+    	Session session = sf.openSession();
+    	
+    	User user = new User();
+    	user.setId(1);
+    	user.setUserName("Pepe");
+    	user.setUserMessage("Hello world from Pepe");
+    	
+    	
+    	
+    	
+    	
+    	session.close();
+    	sf.close();
+    
+    	
+    	
     }
 }
