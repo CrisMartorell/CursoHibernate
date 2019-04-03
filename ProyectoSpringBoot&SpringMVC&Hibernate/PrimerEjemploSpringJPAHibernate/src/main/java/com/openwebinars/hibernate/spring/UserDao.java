4082555592ff378c7670eb96041d821276c77914
Cristina Martorell López <cristina.martorelllopez@plexus.local>
2019-04-03 12:25:46 +0200

    class UserDao done

--- ProyectoSpringBoot&SpringMVC&Hibernate/PrimerEjemploSpringJPAHibernate/src/main/java/com/openwebinars/hibernate/spring/UserDao.java
+++ ProyectoSpringBoot&SpringMVC&Hibernate/PrimerEjemploSpringJPAHibernate/src/main/java/com/openwebinars/hibernate/spring/UserDao.java
@@ -1,8 +1,39 @@
 package com.openwebinars.hibernate.spring;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
 
 @Repository
 @Transactional
 public class UserDao {
-
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void create (User user) {
+		entityManager.persist(user);
+	}
+	
+	public void delete(User user) {
+		if (entityManager.contains(user)) 
+			entityManager.remove(user);
+		else 
+			entityManager.remove(entityManager.merge(user));
+	}
+	
+	public void update(User user) {
+		entityManager.merge(user);
+	}
+	
+	public User getById(int id) {
+		return entityManager.find(User.class, 1);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<User> getAll() {
+		return entityManager.createQuery("select u from User u").getResultList();
+	}
 }
