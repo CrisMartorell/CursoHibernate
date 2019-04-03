1fc4605b78886ec9f154a558ac1bc2347b07a42a
Cristina Martorell López <cristina.martorelllopez@plexus.local>
2019-04-03 11:26:38 +0200

    first part of the course done

---
+++ ProyectoSpringBoot&SpringMVC&Hibernate/PrimerEjemploSpringJPAHibernate/src/main/java/com/openwebinars/hibernate/spring/PrimerEjemploSpringJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.openwebinars.hibernate.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerEjemploSpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerEjemploSpringJpaHibernateApplication.class, args);
+	}
+
+}
