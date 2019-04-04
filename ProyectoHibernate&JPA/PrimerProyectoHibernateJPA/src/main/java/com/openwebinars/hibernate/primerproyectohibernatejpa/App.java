4159ca71cd198bc1db6bfbd3dd9d9a581b8cac79
Cristina Martorell López <cristina.martorelllopez@plexus.local>
2019-04-04 11:17:15 +0200

    some errors solved

--- ProyectoHibernate&JPA/PrimerProyectoHibernateJPA/src/main/java/com/openwebinars/hibernate/primerproyectohibernatejpa/App.java
+++ ProyectoHibernate&JPA/PrimerProyectoHibernateJPA/src/main/java/com/openwebinars/hibernate/primerproyectohibernatejpa/App.java
@@ -4,10 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
